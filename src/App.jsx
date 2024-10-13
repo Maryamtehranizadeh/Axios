be1a553653923eca2b8a4d905f24dbebc4fed3ba
Maryam Tehrani <maryamtehranizadeh@yahoo.com>
2024-10-13 18:54:22 +0100

    initial

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => setTodos(res.data));
+  }, []);
+  return (
+    <>
+      <h1>Axios Library</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
